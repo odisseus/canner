fb466f7cc5b2b78d2c0ea2c7702fc645a4f68ee8
odisseus <odisseus@users.noreply.github.com>
2019-08-05 13:39:17 +0200

    Implemented readable string presentations for FileTree.

--- test/src/canner/FileTree.scala
+++ test/src/canner/FileTree.scala
@@ -13,16 +13,41 @@ object FileTree {
   // Based on https://gist.github.com/charleso/51b6950ecba6c7fd30b49449582d8f48
 
   case class File(contents: List[Byte]) extends FileTree {
+
     override def createAt(location: Path): Unit = Files.write(location, contents.toArray)
+
+    override def toString = {
+      val presentation =
+        if (contents.isEmpty) "<empty>"
+        else if (contents.length <= 16) s""""0x${contents.map(_.toHexString).mkString}""""
+        else s"${contents.length} bytes"
+      s"File($presentation)"
+    }
+
   }
 
   case class Dir(children: Map[String, FileTree]) extends FileTree {
+
     override def createAt(root: Path): Unit = {
       Files.createDirectories(root)
       children.foreach { case (name, child) =>
         child.createAt(root.resolve(name))
       }
     }
+
+    private def maxNesting: Int = (children.values.map{
+      case _: File => 0
+      case x: Dir => 1 + x.maxNesting
+    } ++ List(0)).max
+
+    override def toString = {
+      val presentation =
+        if (children.isEmpty) "<empty>"
+        else if (children.size <= 16) children.map{ case (name, child) => s"$name -> $child" }.mkString(", ")
+        else s"${children.size} children, $maxNesting levels"
+      s"Dir ($presentation)"
+    }
+
   }
 
   private def filename: Gen[String] = nonEmptyListOf(alphaLowerChar).map(_.mkString)
